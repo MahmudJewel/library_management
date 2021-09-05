a8c6c8e4960c58a65338df5f0a7f26b0408b09dd
villain <villain@kali.local>
2021-09-06 00:13:45 +0600

    static files are added

--- library_management/settings.py
+++ library_management/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'admn',
+    'student',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'library_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#start Added by me
+STATICFILES_DIRS = [   #Search during collectstatic
+    os.path.join(BASE_DIR,'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'djangoAutoStatic') #collectstatic==> automatically created 'djangoStatic' folder.
+MEDIA_ROOT=os.path.join(BASE_DIR,'static') #for imagefield
+#End added
+
